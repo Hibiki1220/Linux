a993b0625a73c168901ac29e8f6dc4a9924b7731
Hibiki1220 <123935680+Hibiki1220@users.noreply.github.com>
2024-05-31 23:02:43 +0900

    Update kadai_if2.sh

--- kadai_if2.sh
+++ kadai_if2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 i=`expr $RANDOM % 100`
 echo -e "数当てゲームの始まり！\n\nコンピュータが考えた数値を当てましょう(10回のチャンス)"
-
 count=0
-#countが10でなければ処理を繰り返す
+
+#countが11でなければ処理を繰り返す。10じゃない理由は数字を比較した後にcountを+1しているので、「10回目が終わる→22行目でcountが10になる→
+#8行目で11ではないので10行目のチェックに進む→countが10なのでechoを出力→if終了して8行目で判定→11なのでwhileから抜けて処理終了」という流れをとるため
 while [ $count -ne 11 ];do
 #処理を始めるごとにcountの数を1ずつ増やしておく
-        count=`expr $count + 1`
-    if [ $count -eq 11 ];then
+    if [ $count -eq 10 ];then
         echo "10回入力しました。あなたの負けです"
     else
         read -p "0~99の数値を入力してください:" num
@@ -20,4 +20,5 @@ while [ $count -ne 11 ];do
             break
         fi
     fi
+    count=`expr $count + 1`
 done
